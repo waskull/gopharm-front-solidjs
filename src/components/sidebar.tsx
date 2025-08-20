a3a3b0145782d207aa5774dc4bc5ab8d65c6e601
Martin Castillo <58961096+waskull@users.noreply.github.com>
2025-08-20 15:36:24 -0400

    rename sidebar appname

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -43,7 +43,7 @@ export default function Sidebar({ toggleSidebar }: { toggleSidebar: Accessor<boo
     return (
         <div class={`${toggleSidebar() ? " hidden " : " block "} w-64 bg-gray-800 fixed h-full border-r border-gray-600 px-4 overflow-y-auto z-10`}>
             <div class="my-2 mb-4">
-                <h1 class="text-2xl text-white tiktok-font text-center border-b-2 pb-3 mt-5 font-bold">GOPHARM</h1>
+                <h1 class="text-2xl text-white tiktok-font text-center border-b-2 pb-3 mt-5 font-bold">SOLIDJS-CRUD-TS</h1>
             </div>
             <div>
                 <ul class="mt-4 text-white font-semibold">
